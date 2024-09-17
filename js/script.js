4e58a9086dd1039f3b328719d00a3f8ef99c2593
ayrtonSmach <aysoaresmachado@gmail.com>
2024-09-17 20:09:51 -0300

    add function to info buttons on header

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const list = document.getElementById('list');
     const btn_close = document.getElementById('btn_close');
-    const btn_info = document.querySelector('.btn_info button');
+    const btn_info = document.querySelectorAll('.btn_info button');
 
-    openCloseList();
+    openCloseInfo(btn_info);
+    openCloseList(btn_close);
 });
 
-
-
-function openCloseInfo() {
-    
+function openCloseInfo(btn) {
+    btn.forEach(btn => {
+        btn.addEventListener('click', () => {
+            card = document.getElementById(`${btn.id}_content`);
+            console.log(card);
+            if (card.style.display === 'none' || card.style.display === '') {
+                card.style.display = 'block';
+            }
+            else {
+                card.style.display = 'none';
+            }
+        });
+    });
 }
 
-function openCloseList() {
-    btn_close.addEventListener('click', function() {
+function openCloseList(btn) {
+    const list = document.getElementById('list');
+    btn.addEventListener('click', () => {
         if (list.style.display === 'none' || list.style.display === '') {
             list.style.display = 'block';
         }
