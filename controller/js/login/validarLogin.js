dfd8b204d2a0d07b79a85f3c9c4723447af068a9
ayrtonSmach <ayrtonsoaresm@gmail.com>
2024-10-08 14:13:50 -0300

    add mvc and login responsivity

--- controller/js/login/validarLogin.js
+++ controller/js/login/validarLogin.js
@@ -5,9 +5,8 @@ export function validarLogin() {
     let passwordField = document.getElementById('password')
 
     console.log(emailField, passwordField)
-    
     if(emailField.value === "admin" && passwordField.value === "admin") {
-        window.location.href ='./../index.html'
+        window.location.href = 'index.html'
     } else {
         errorInput(emailField, passwordField);
     }
